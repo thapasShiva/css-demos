1da0f4684d7696cf64a1746d3a2c13261806a704
Thapas <thapasvi.shiva@gmail.com>
2021-01-16 09:40:57 +0530

    Added Login Screen
    Added Login Screen
    Removed Default app screen in App.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CssLoginScreenComponent } from './screens/css-login-screen/css-login-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CssLoginScreenComponent
   ],
   imports: [
     BrowserModule,
